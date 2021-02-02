6b3c5df77c3d613068708cc3d5f75e84416ed193
Ron Xu <59843980+R0n12@users.noreply.github.com>
2021-02-02 17:20:36 -0500

    update default batch size to 64

--- examples/pytorch/pytorch_synthetic_benchmark.py
+++ examples/pytorch/pytorch_synthetic_benchmark.py
@@ -16,7 +16,7 @@ parser.add_argument('--fp16-allreduce', action='store_true', default=False,
 
 parser.add_argument('--model', type=str, default='resnet50',
                     help='model to benchmark')
-parser.add_argument('--batch-size', type=int, default=32,
+parser.add_argument('--batch-size', type=int, default=64,
                     help='input batch size')
 
 parser.add_argument('--num-warmup-batches', type=int, default=10,
